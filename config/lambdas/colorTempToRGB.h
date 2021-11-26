46f989bdee42ed9488c155a6b47d73fdae58022d
h4de5 <h4de5@users.noreply.github.com>
2021-11-26 01:50:12 +0100

    Try to fix errors - remove most effects

--- config/lambdas/colorTempToRGB.h
+++ config/lambdas/colorTempToRGB.h
@@ -5,58 +5,58 @@
  */
 #include "esphome.h"
 
-int clamp( int x, int min, int max ) {
+int clamp(int x, int min, int max) {
 
-    if(x<min){ return min; }
-    if(x>max){ return max; }
+  if (x < min) {
+    return min;
+  }
+  if (x > max) {
+    return max;
+  }
 
-    return x;
+  return x;
 }
 
-ESPColor colorTemperatureToRGB(int kelvin) {
+Color colorTemperatureToRGB(int kelvin) {
 
-    int temp = kelvin / 100;
+  int temp = kelvin / 100;
 
-    int red, green, blue;
+  int red, green, blue;
 
-    if( temp <= 66 ){ 
+  if (temp <= 66) {
 
-        red = 255; 
-        
-        green = temp;
-        green = 99.4708025861 * log(green) - 161.1195681661;
+    red = 255;
 
-        
-        if( temp <= 19){
+    green = temp;
+    green = 99.4708025861 * log(green) - 161.1195681661;
 
-            blue = 0;
+    if (temp <= 19) {
 
-        } else {
-
-            blue = temp-10;
-            blue = 138.5177312231 * log(blue) - 305.0447927307;
-
-        }
+      blue = 0;
 
     } else {
 
-        red = temp - 60;
-        red = 329.698727446 * pow(red, -0.1332047592);
-        
-        green = temp - 60;
-        green = 288.1221695283 * pow(green, -0.0755148492 );
-
-        blue = 255;
+      blue = temp - 10;
+      blue = 138.5177312231 * log(blue) - 305.0447927307;
 
     }
 
+  } else {
 
-    return ESPColor(
-	    clamp(red,   0, 255),
-        clamp(green, 0, 255),
-        clamp(blue,  0, 255)
-    );
+    red = temp - 60;
+    red = 329.698727446 * pow(red, -0.1332047592);
+
+    green = temp - 60;
+    green = 288.1221695283 * pow(green, -0.0755148492);
+
+    blue = 255;
+
+  }
+
+  return Color(
+    clamp(red, 0, 255),
+    clamp(green, 0, 255),
+    clamp(blue, 0, 255)
+  );
 
 }
-
-
